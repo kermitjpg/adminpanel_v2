47cfd689979ab7d0cabb7dc1e6c2cae307dac862
Kermit <111296194+kermitjpg@users.noreply.github.com>
2023-03-29 23:59:43 +0300

    Yönetim Paneli

--- admin/inc/ayar.php
+++ admin/inc/ayar.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
-$host     = '127.0.0.1'; //localhost
-$user     = 'root';
-$pass     = 'Biskuvi123';
-$db        = 'midyepanel_v2';
+$host     = 'host'; //localhost
+$user     = 'kullaniciAdi';
+$pass     = 'databaseSifre';
+$db        = 'databaseAdi';
 $baglan = mysqli_connect($host, $user, $pass, $db) or die(mysqli_Error());
 mysqli_query($baglan, "SET CHARACTER SET 'utf8'");
 mysqli_query($baglan, "SET NAMES 'utf8'");
